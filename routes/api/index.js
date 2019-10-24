f988c0cf047196ce2c038bf2b52d5504bdb6a31a
Elizabeth Bodzy <bodze087@newschool.edu>
2019-10-24 18:54:59 -0500

    configured files and started adding code

---
+++ routes/api/index.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const bookRoutes = require("./books");
+const googleRoutes = require("./google");
+
+// Book routes
+router.use("/books", bookRoutes);
+
+// Google routes
+router.use("/google", googleRoutes);
+
+// If no API routes are hit, send the React app
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+  
+  module.exports = router;
