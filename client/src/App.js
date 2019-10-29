66c608a383f825ef997e4fe4874d8d3549c201a3
Elizabeth Bodzy <bodze087@newschool.edu>
2019-10-29 15:13:28 -0500

    pushing changes, but running into npm start error

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
+import Save from "./pages/save";
+import Search from "./pages/search";
+import NotFound from "./pages/notFound";
+import NavBar from "./components/NavBar";
+
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+
+    </>
   );
 }
 
