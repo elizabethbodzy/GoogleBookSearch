a2436ca36c883979f6350e89e01264613eb83f9b
Elizabeth Bodzy <bodze087@newschool.edu>
2019-10-29 17:19:06 -0500

    setting up Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,30 @@
-import React from 'react';
-import Save from "./pages/save";
-import Search from "./pages/search";
+import React, { Component } from 'react';
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Saved from "./pages/Save";
+import Books from "./pages/Search";
 import NotFound from "./pages/notFound";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-
 
 // import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <>
-      <NavBar />
-
-      <Footer />
-
-    </>
+    // <Router>
+      <div>
+        <NavBar />
+        {/* <Switch>
+        <Route exact path = "/" component = {Books} />
+          <Route exact path = "/books" component = {Books} />
+          <Route exact path = "/books/:id" component = {Saved} />
+          <Route component = {NotFound} />
+        </Switch> */}
+        <Footer />
+        
+      </div>
+    // </Router>
+   
   );
 }
 
